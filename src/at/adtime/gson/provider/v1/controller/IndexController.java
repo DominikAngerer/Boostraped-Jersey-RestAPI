d036cb80b7072bc668bab642802a4974f8691552
Dominik <git@adtime.at>
2015-01-24 22:10:55 +0100

    Create IndexController.java
    Added Controller directly over Github because the 404 annoyed the hell out of me :)

---
+++ src/at/adtime/gson/provider/v1/controller/IndexController.java
@@ -0,0 +1,18 @@
+package at.adtime.gson.provider.v1.controller;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class ResourceController {
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response get() {
+		return Response.status(200).entity("Okay, I hated the 404 Page here ;)").build();
+	}
+}
